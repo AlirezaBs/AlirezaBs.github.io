e009b8a9635c5725ca6ed87aa19a338205e235c9
Alireza <aalirezabs@gmail.com>
2021-11-09 16:20:58 +0330

    service section compleated and navbar shrink upgraded

---
+++ script/script.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("main-nav").classList.add('scrolled-nav');
+    document.getElementById("main-nav").classList.remove("myNav");
+  } else {
+    document.getElementById("main-nav").classList.remove('scrolled-nav');
+    document.getElementById("main-nav").classList.add("myNav");
+
+  }
+}
