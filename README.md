17ede26ff12f579455dbc3193c7cd7c9d0969f28
Alireza <aalirezabs@gmail.com>
2021-12-06 16:04:21 +0330

    animation on scroll added to project

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Hello world :)
+### Hello world :)
+> This is my first landing page built by myself
 
-This is my first landing page built by myself
-
-to see demo -->  https://alirezabs.github.io/my-first-landing-page/
+to see demo of the project click [there](https://alirezabs.github.io/my-first-landing-page/).
--- script/script.js
+++ script/script.js
@@ -4,11 +4,13 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("main-nav").classList.add('scrolled-nav');
+    document.getElementById("main-nav").classList.add("scrolled-nav");
     document.getElementById("main-nav").classList.remove("myNav");
   } else {
-    document.getElementById("main-nav").classList.remove('scrolled-nav');
+    document.getElementById("main-nav").classList.remove("scrolled-nav");
     document.getElementById("main-nav").classList.add("myNav");
-
   }
 }
+
+
+ 
